7e403c8219f7a8d3609d52a3b455cc947c143347
Falko Zurell <falko.zurell@ubirch.com>
2018-09-17 21:44:47 +0200

    changed from ubirch-java Container to OpenJDK

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubirch/java
+FROM debian:stretch
 MAINTAINER Falko Zurell <falko.zurell@ubirch.com>
 
 ENV SBT_VERSION 0.13.13
@@ -7,10 +7,16 @@ ENV PATH ${PATH}:${SBT_HOME}/bin
 ENV VCS_REF $VCS_REF
 ENV BUILD_DATE $BUILD_DATE
 ENV sbt.ivy.home /build/.ivy2
+LABEL description="ubirch SBT tool container"
 
-
+RUN apt-get update
+RUN apt-get --fix-missing install curl openjdk-8-jdk -y && \
+apt-get autoclean && apt-get --purge -y autoremove && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 # Install sbt
 # https://dl.bintray.com/sbt/native-packages/sbt/0.13.13/sbt-0.13.13.tgz
+#INCREASE LINUX OPEN FILE HANDLES
+# RUN echo 800000 > /proc/sys/fs/file-max
 RUN mkdir -p $SBT_HOME
 RUN curl -sL "http://dl.bintray.com/sbt/native-packages/sbt/${SBT_VERSION}/sbt-${SBT_VERSION}.tgz" | tar -xvz --strip-components 1 -C $SBT_HOME && \
     echo -ne "- with sbt $SBT_VERSION\n" >> /root/.built
--- build.sh
+++ build.sh
@@ -22,17 +22,17 @@ function fix_dockerfile_version() {
 # build the docker container
 function build_container() {
 
-    fix_dockerfile_version
-	
-	if [ "v${GO_PIPELINE_LABEL}" = "v" ]; then	
+    # fix_dockerfile_version
+
+	if [ "v${GO_PIPELINE_LABEL}" = "v" ]; then
 		PUBLISH_VERSION="latest"
 	else
-		PUBLISH_VERSION="v${GO_PIPELINE_LABEL}"
+		PUBLISH_VERSION="vOpenJDK_${GO_PIPELINE_LABEL}"
 	fi
 
     echo "Building SBT container"
 
-    mkdir -p VAR && docker build -t ubirch/${CONTAINER_NAME}:${PUBLISH_VERSION} -f Dockerfile.v${GO_PIPELINE_LABEL} .
+    mkdir -p VAR && docker build -t ubirch/${CONTAINER_NAME}:${PUBLISH_VERSION} -f Dockerfile .
 
 
     if [ $? -ne 0 ]; then
@@ -44,22 +44,22 @@ function build_container() {
 
 # publish the new docker container
 function publish_container() {
-		
-		
-	if [ "v${GO_PIPELINE_LABEL}" = "v" ]; then	
+
+
+	if [ "v${GO_PIPELINE_LABEL}" = "v" ]; then
 		PUBLISH_VERSION="latest"
 	else
 		PUBLISH_VERSION="v${GO_PIPELINE_LABEL}"
 	fi
-		
-	
+
+
   echo "Publishing Docker Container with version: ${PUBLISH_VERSION}"
-  docker push ubirch/${CONTAINER_NAME}:${PUBLISH_VERSION} 
+  docker push ubirch/${CONTAINER_NAME}:${PUBLISH_VERSION}
     if [ $? -ne 0 ]; then
         echo "Could not push ubirch/${CONTAINER_NAME}:${PUBLISH_VERSION} to Docker hub"
         exit 1
     fi
-  docker tag ubirch/${CONTAINER_NAME}:${PUBLISH_VERSION} ubirch/${CONTAINER_NAME}:latest 
+  docker tag ubirch/${CONTAINER_NAME}:${PUBLISH_VERSION} ubirch/${CONTAINER_NAME}:latest
   docker push ubirch/${CONTAINER_NAME}:latest
 
   if [ $? -eq 0 ]; then
